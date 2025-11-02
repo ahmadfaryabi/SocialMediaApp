bfa18538dff017e621e984d237ddea43027d9d18
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2025-11-02 22:33:04 +0100

    noe kodeendringer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using SocialMediaApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
